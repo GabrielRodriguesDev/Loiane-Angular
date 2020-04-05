17d049d8fd6f5e40733464cc8b1695d480ea90e0
Gabriel Silva Rodrigues Mota <gabriel>
2020-04-04 22:30:22 -0300

    Curso Angular #26 - #27 - #28  Foram vistas as diretivas ngSwitch && > > ngFor o motivo de usar o  asteristico  nas diretivas

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule,
