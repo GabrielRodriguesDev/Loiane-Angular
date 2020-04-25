f82f704d6a386d1c99d03d9fa3d31939b690a53e
Gabriel Silva Rodrigues Mota <gabriel>
2020-04-24 22:37:58 -0300

    Revert "Curso Angular #39 Injeção de Dependência + Como usar um serviço em um componente"
    This reverts commit 83917cf9d170efdef5b3cd0877aa3a1b66464a0f.

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,18 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoService } from './cursos/curso.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [CursoService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
