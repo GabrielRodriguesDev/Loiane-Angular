83917cf9d170efdef5b3cd0877aa3a1b66464a0f
Gabriel Silva Rodrigues Mota <gabriel>
2020-04-24 22:21:57 -0300

    Curso Angular #39 Injeção de Dependência + Como usar um serviço em um componente

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoService } from './cursos/curso.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CursoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
