41f942cdbdf051f1699b8ba9f3c5b88d57563650
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2020-05-03 23:15:33 -0300

    Curso Angular #46 Criando um pipe Puro - #47 Criando um pipe Impuro - #48 Pipe Async (Assincrono)

--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -16,10 +16,35 @@ export class ExemplosPipesComponent implements OnInit {
     url:'https://www.amazon.com.br/Learning-JavaScript-Data-Structures-Algorithms-ebook/dp/B01C2XX8Y2/ref=sr_1_3?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=Learning+JavaScript+Data+Structures+and+Algorithms&qid=1588450518&sr=8-3' 
   }
 
+  livros: string [] = ['Angular','Java', 'C#', 'Kotlin' ]
+
+  filtro : string;
+
   
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  newValue(value: string){
+     this.livros.push(value)
+  }
+
+  getCursos( ){
+    if(this.livros.length === 0 || this.filtro === undefined) {
+      return this.livros
+    }
+    return this.livros.filter((v) => {
+      if(v.toLocaleLowerCase().indexOf(this.filtro.toLocaleLowerCase()) >= 0){
+        return true
+      }
+      return false
+    })
+  } /*Não entendi como funciona o filtro muito bem*/
+
+  valueAsync = new Promise ((resolve, reject) => {
+    setTimeout(() => resolve('Valor assincrono'), 2000);
+  })
+
 
 }
