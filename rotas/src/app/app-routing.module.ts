51717f0e38c4e3e8378f23b09f8855e617118b7b
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2020-05-06 23:53:48 -0300

    Curso Angular #50: Rotas: Configurando rotas simples

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component'
+import { CursosComponent } from './cursos/cursos.component';
 
-const routes: Routes = [];
 
-@NgModule({
+const routes: Routes = [ //Const que contém as rotas e seus redirecionamentos
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'cursos', component: CursosComponent}
+];
+
+@NgModule({ //Arquivos de configuração das rotas
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } //Precisa ser exportado para poder ser importado como 'import', no app module
