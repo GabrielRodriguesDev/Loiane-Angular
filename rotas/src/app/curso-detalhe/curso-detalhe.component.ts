213e95f22e004fd97f832e089dd39e4853386a94
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-05-13 22:52:45 -0300

    Curso Angular #54: Rotas: Escutando mudanças nos parâmetros de roteamento

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private route: ActivatedRoute) {  //Instacia da classe do angular que nós trás as informações das rotas, onde podemos acessar os parametros por exemplo.
+    console.log(this.route.snapshot.params)
+    this.id = this.route.snapshot.params['id']; //Como 'paramns' é um objeto dinamico estamos acessando o atributo como se fosse uma chave
+  } 
+
 
   ngOnInit(): void {
   }
