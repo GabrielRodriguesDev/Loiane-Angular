0c47c355cc2e2517a86f3144aa7f4e5c9cf3e24d
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-05-17 23:33:42 -0300

    Curso Angular #56: Rotas: Definindo e extraindo parâmetros de url (query)

--- rotas/src/app/app.component.ts
+++ rotas/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rotas';
+
+  params: number
+
+  constructor(
+
+    private router: Router
+  ){
+
+  }
+  setParamsPaggination(params: number){
+    this.params = params //Pegando o valor passado pelos paggination
+
+    this.router.navigate(['Cursos'],
+    {queryParams: {'pagina': params}})//Atualizando a rota junto o queryParams, após passarem receber o valor do paggination  
+  }
 }
--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CursosService } from './cursos.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -10,13 +11,32 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any [];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor( private CursosService: CursosService) { 
-    
+  constructor( 
+    private CursosService: CursosService,
+    private ActivatedRoute: ActivatedRoute
+    ) { 
+    console.log(this.ActivatedRoute.queryParams)
   }
 
   ngOnInit(): void {
     this.cursos = this.CursosService.getCursos();
+
+    this.inscricao = this.ActivatedRoute.queryParams.subscribe( //A cada carregamento da classe está sendo feito a inscrição no BehaviorSubject esperando a mudança no 'paginas', (Página que foi passada na rota) e colocando na variavel.;
+       (queryParams: any) => {
+         this.pagina = queryParams['pagina'];
+       }
+    )
+
+    console.log(this.pagina)
   }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe //Como boa pratica, na destruição do componente também cancelar a inscrição, pois não temos mais interesse nela. 
+    
+  }
+  
 
 }
