309699c1074e277e89c9fc8653d1b69514adb18f
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-05-24 01:05:45 -0300

    Curso Angular #60: Rotas Filhas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+    
 
   ],
   imports: [
     BrowserModule,
     CursosModule,
-    AppRoutingModule
+    AlunosModule,
+    AppRoutingModule,
+    
   ],
 
 
