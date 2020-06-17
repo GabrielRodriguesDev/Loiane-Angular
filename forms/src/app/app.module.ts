0c5815111d7da59023893edbc837c11c91294f90
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-06-16 23:25:04 -0300

    Curso Angular #74: Forms (template driven) Controles ngForm, ngSubmit e ngModel  Curso Angular #75: Forms (template driven) Inicializando valores com ngModel  Curso Angular #76: Forms (template driven) Módulos e FormsModule  Curso Angular #77: Forms (template driven) Aplicando validação nos campos

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent,
     DataFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    //Module
+    TemplateFormModule,
+    //Rounting
     AppRoutingModule
   ],
   providers: [],
--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  user: any = {
+    name: 'Gabriel',
+    email: 'gabriel@onsist.com.br'
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    console.log(form);
+    console.log(this.user)
   }
 
 }
