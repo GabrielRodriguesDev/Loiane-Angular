4d09643e7440021ae0a64fb48242d468b5990669
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-06-22 22:40:41 -0300

    Curso Angular #92: Formulários reativos: Fazendo submit

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-data-form',
@@ -10,26 +12,30 @@ export class DataFormComponent implements OnInit {
 
   form: FormGroup 
 
-  constructor( private formBuilder: FormBuilder) { }
+  constructor( 
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+    ) { }
 
   ngOnInit() {
-    
     /*this.form = new FormGroup({ 
       name: new FormControl('Gabriel'),
       email: new FormControl()
-
     })*/ //Forma mais verbosa de iniciar um Data-Form.
 
-    this.form = this.formBuilder.group({ //Grupo onde possui os controles individuais de cada form.
+    this.form = this.formBuilder.group({ // Objeto de FormGroup  -> Grupo onde possui os controles individuais de cada form.
       name: ['Valor inicial'], //Todo o campo é um controle de grupo.
       email: [null],
-
     })
   }
 
-
-  changed(){
+  onSubmit(){
     console.log(this.form)
+    this.httpClient.post('https://httpbin.org/post', JSON.stringify(this.form.value))//Transformando o valo passando em JSON. 
+    .subscribe(data => {console.log(data)})//Se inscrevendo no POST, e quando obter um "REPONSE" fazer um console log com oq foi retornado 
   }
+ 
+
+
 
 }
