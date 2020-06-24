0053ddbf5381897570dbf9547725e91bd8c16e2d
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-06-23 23:08:34 -0300

    Curso Angular #94: Formulários reativos: Aplicando validação nos campos

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
@@ -25,14 +25,13 @@ export class DataFormComponent implements OnInit {
     })*/ //Forma mais verbosa de iniciar um Data-Form.
 
     this.form = this.formBuilder.group({ // Objeto de FormGroup  -> Grupo onde possui os controles individuais de cada form.
-      name: ['Valor inicial'], //Todo o campo é um controle de grupo.
-      email: [null],
+      name: ['Valor inicial', [Validators.required, Validators.minLength(3)]], //Todo o campo é um controle de grupo.
+      email: [null, [Validators.required, Validators.email]], //Validação atráves do Validator (Forma de validação atráves do Reactive Forms), podemos ter mais de um validators, só precisa adicionar as validação dentro do array.
     })
   }
 
   onSubmit(){
-    console.log(this.form)
-    this.httpClient.post('https://htatpbin.org/post', JSON.stringify(this.form.value))//Transformando o valo passando em JSON. 
+    this.httpClient.post('https://httpbin.org/post', JSON.stringify(this.form.value))//Transformando o valo passando em JSON. 
     .subscribe(data => { //Se inscrevendo no POST, e quando obter um "REPONSE" fazer um console log com oq foi retornado 
       console.log(data)
       //Reset Form se não houver erro.
@@ -40,7 +39,7 @@ export class DataFormComponent implements OnInit {
         this.resetForm()
       }
     },
-    (error: Error)=> {
+    (error: Error)=> {//Recebendo o erro e usando para a logica do IF acima.
       this.erro = true;
       alert("Erro ao enviar fórmulario")
     });
