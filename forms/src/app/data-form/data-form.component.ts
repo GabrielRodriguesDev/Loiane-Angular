4075413f2edbf9367802a5b17490300704fa8f78
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-06-25 23:46:21 -0300

    Curso Angular #96: Formulários reativos: Endereço (migrando de template driven para form reativo)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -25,11 +24,19 @@ export class DataFormComponent implements OnInit {
     })*/ //Forma mais verbosa de iniciar um Data-Form.
 
     this.form = this.formBuilder.group({ // Objeto de FormGroup  -> Grupo onde possui os controles individuais de cada form.
-      name: ['Valor inicial', [Validators.required, Validators.minLength(3)]], //Todo o campo é um controle de grupo.
-      email: [null, [Validators.required, Validators.email]], //Validação atráves do Validator (Forma de validação atráves do Reactive Forms), podemos ter mais de um validators, só precisa adicionar as validação dentro do array.
+      name: [null, [Validators.required, Validators.minLength(3)]], //Todo o campo é um controle de grupo.
+      email: [null, [Validators.required, Validators.email]], //Validação atráves do Validator (Forma de validação atráves do Reactive Forms), podemos ter mais de um validators, só precisa adicionar as validação dentro do array.    
+      cep: [null, Validators.required],
+      number: [null, Validators.required],
+      street: [null, Validators.required],
+      complemento: null,
+      neighborhood: [null, Validators.required],
+      city: [null, Validators.required],
+      state: [null, Validators.required],
     })
   }
 
+  //Form
   onSubmit(){
     this.httpClient.post('https://httpbin.org/post', JSON.stringify(this.form.value))//Transformando o valo passando em JSON. 
     .subscribe(data => { //Se inscrevendo no POST, e quando obter um "REPONSE" fazer um console log com oq foi retornado 
@@ -47,6 +54,26 @@ export class DataFormComponent implements OnInit {
  
   resetForm(){
     this.form.reset()
+  }
+
+  //Formatting CSS
+
+  checkValidEmail(){
+    let fielEmail = this.form.get('email')
+    if(fielEmail.errors){
+      return fielEmail.errors['email'] && fielEmail.touched
+    }
+  }
+
+  checksValidTouched(field){
+    return !this.form.get(field).valid && this.form.get(field).touched // Verifica se o campo do válido de acordo com a condição passada; (Reactive forms já temos os dados no componente por isso é diferente do template drive)
+  }
+
+
+  errorStyle(field){
+    return {
+      'was-validated ': this.form.get(field).valid && this.form.get(field).touched
+    }   
   }
 
 
