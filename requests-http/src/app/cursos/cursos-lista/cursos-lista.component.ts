7fa1eb48ecc22920a683aa86899933552a81369b
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-07-25 16:10:24 -0300

    Curso Angular #122: Http GET + Pipe Async

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from 'src/app/models/curso';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -10,14 +11,18 @@ import { Curso } from 'src/app/models/curso';
 })
 export class CursosListaComponent implements OnInit {
 
-  cursos: Curso[];
+  /*cursos: Curso[];*/
+
+  cursos$: Observable <Curso[]> //Declaração de variavel Filandesa (Quando uma varivel tiver o DOLLAR significa que é um observable (Prática da comunidade))
 
   constructor(
     private cursosService : CursosService
   ) { }
   
   ngOnInit() {
-    this.cursosService.list()
-      .subscribe(data => this.cursos = data)
+    /*this.cursosService.list()
+      .subscribe(data => this.cursos = data)*/
+
+    this.cursos$ = this.cursosService.list();
   }
 }
