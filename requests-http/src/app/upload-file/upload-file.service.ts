f0946cec0c1c9cd9b8626059a408dfc986975edf
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-08-10 22:41:49 -0300

    Curso Angular #137: Http Upload de Arquivo: Request com FormData

---
+++ requests-http/src/app/upload-file/upload-file.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { fromEventPattern } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFileService {
+
+  constructor(
+    private httpClient : HttpClient
+  ) { }
+
+  upload(files: Set<File>, url: string) { 
+    
+    
+    const formData = new FormData(); // Instaciando um formData
+    files.forEach(file => formData.append('file', file, file.name)); // Fazendo um forEach usando o append()
+
+
+        // Exemplo de request manual no servidor
+    //const request = new HttpRequest('POST', url, formData);
+    //return this.httpClient.request(request);
+        
+        // Exemplo com o HttpClient
+      return this.httpClient.post(url, formData) // Retornando um post sobre os parametros para quem chamou o método
+  }
+}
--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files: Set<File>; // A estrutua de dados 'Set' já cuida dos arquivos não deixando importar mais de um do mesmo.
+
+  constructor(
+    private uploadFileService: UploadFileService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -22,11 +27,21 @@ export class UploadFileComponent implements OnInit {
     //Lindando com mutiplos arq
     const filesName = []; // Criando const de array vazia
     
+    this.files = new Set(); // Instanciado a estrutura de dados Set na variavel 
+    
     for(let i = 0; i<selectFiles.length; i++){ // Fazendo um laço que percorre para cada nome de arq existente
       filesName.push(selectFiles[i].name); // A cada arq existente, empurra no filesName Array.
+      this.files.add(selectFiles[i]);
     }
     document.getElementById('customFileLabel').innerHTML = filesName.join(', '); /* Depois de popular todo o filesName,
     (Js puro) seto no campo da label os registros do array filesName fazendo um join (juntando) todos separando com virgula e espaço*/
+  }
+
+  onUpload() {
+    if(this.files && this.files.size > 0) { // Verficando se o valor existe
+      this.uploadFileService.upload(this.files, 'http://localhost:8000/upload') // Chamando o serviço e passando os parametros necessários.
+      .subscribe(reponse => console.log('Upload Concluido'))// Pegando a repose e criando um console.
+    } // Nesse caso é necessário fazer ums unsubscribe.
   }
 
 }
