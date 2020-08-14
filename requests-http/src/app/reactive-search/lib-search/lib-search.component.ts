25bcbb53c52139fc03fc1fc8b275ec2ab878dcab
Gabriel Rodrigues <you@example.comgabriel.rodrigues.mota@outlook.com>
2020-08-13 23:41:02 -0300

    Curso Angular #143: Http: Passando Parâmetros na URL (HttpParams)

--- requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
+++ requests-http/src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClientModule, HttpClient, HttpParams } from '@angular/common/http';
+import { tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -9,14 +12,37 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent implements OnInit {
 
   queryFields = new FormControl();
+  SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$: Observable<any>;
+  total: number;
 
-  constructor() { }
+  constructor(
+    private httpClient : HttpClient
+  ) { }
 
   ngOnInit() {
   }
 
   onSearch() {
-    console.log(this.queryFields.value);
-  }
+    const fields =  'name,version'
+    let value = this.queryFields.value;
+    if(value && (value = value.trim()) != '') { // Validações (Se o user não digitou nada ou a busca é vazia, ela caira fora do if)
+    
+      const params_ = { // Passando Parametros a URL através de um objeto
+        search: value,
+        fields: fields
+      }
 
+      let params = new HttpParams(); // Passando parametros a URL através do HttpParams (Mais usado para paramtros dinamicos)
+      params = params.set('search', value); // Como é um valor imutavél temos que passar o valor pra ele sempre (É preguiçoso)
+      params = params.set('fields', fields)
+
+      this.results$ = this.httpClient
+    .get(this.SEARCH_URL, { params })// Fazendo um get no link passado e colocando dentro da variavel que é um Observable, passando o link e os Parametros de rota através de parametros de rotas 
+      .pipe(
+        tap((resp: any) => (this.total = resp.total)), // Pegando o total da Busca
+        map((resp: any) => resp.results) // Pegando o resultados da busca
+      );
+    }
+  }
 }
